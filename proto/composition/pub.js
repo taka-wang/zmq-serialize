f53026c5c0d6eb3ea418e11f6acbba463cd3855e
Taka Wang <wang.chia.ming@gmail.com>
2016-03-02 13:03:03 +0800

    refine pub.js

--- proto/composition/pub.js
+++ proto/composition/pub.js
@@ -4,21 +4,25 @@ var zmq = require('zmq')
 , messages = protobuf(fs.readFileSync('type.proto'))
 , pub = zmq.socket('pub')
 , command = {
-    {"receiver": "mbtcp",
-    "sender":   "restful",        
-    "version":  "1",
-    "tid":      33,                
-    "method":   "mbtcp.once.write"},
-    
-    {"ip": "192.168.1.1", 
-    "port": 503,
-    "id": 22},
-    
-    {"code":     1,                 
-    "register": 2003,             
-    "value":    "1025",            
-    "type":     "int64",        
-    "alias":    "hello_1"   }
+    "cmd_header":{
+    	"receiver": "mbtcp",
+    	"sender":   "restful",        
+    	"version":  "1",
+    	"tid":      33,                
+    	"method":   "mbtcp.once.write"
+    },
+    "mb_tcp_header":{
+    	"ip": "192.168.1.1", 
+    	"port": 503,
+    	"id": 22
+    },
+    "mb_write_request":{
+    	"code":     1,                 
+    	"register": 2003,             
+    	"value":    "1025",            
+    	"type":     "int64",        
+    	"alias":    "hello_1"
+    }
 }
 , buf = messages.MbTcpSingleWriteReq.encode(command); // encode
 
