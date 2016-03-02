da0f662ad049c59cc91f94bf3da5fb9a45f7acf1
Taka Wang <wang.chia.ming@gmail.com>
2016-03-02 13:29:18 +0800

    implement pub/sub for protobuf

---
+++ proto/repeat/pub.js
@@ -0,0 +1,43 @@
+var zmq = require('zmq')
+, protobuf = require('protocol-buffers')
+, fs = require('fs')
+, messages = protobuf(fs.readFileSync('type.proto'))
+, pub = zmq.socket('pub')
+, command = {
+    "cmd_header":{
+        "receiver": "mbtcp",
+        "sender":   "restful",        
+        "version":  "1",
+        "tid":      33,                
+        "method":   "mbtcp.once.write"
+    },
+    "mb_tcp_header":{
+        "ip": "192.168.1.1", 
+        "port": 503,
+        "id": 22
+    },
+    "requests": [ 
+        {
+            "code":     1,                 
+            "register": 2003,             
+            "value":    "1025",            
+            "type":     "int64",        
+            "alias":    "hello_1"  
+        }, 
+        {
+            "code":     1,                 
+            "register": 2003,             
+            "value":    "1025",            
+            "type":     "int64",        
+            "alias":    "hello_1"  
+        }
+    ]
+}
+, buf = messages.MbTcpSingleWriteReq.encode(command); // encode
+
+pub.connect("ipc:///tmp/dummy"); // connect to zmq endpoint
+
+setInterval(function() {
+    pub.send("mbtcp.once.write", zmq.ZMQ_SNDMORE);
+    pub.send(buf);
+}, 1000); // emit every 1 seconds
