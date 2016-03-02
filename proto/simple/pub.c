261bde71bcf5505666a7f1fd6eb4915b60cdde99
Taka Wang <wang.chia.ming@gmail.com>
2016-03-02 15:30:55 +0800

    rearrange

--- proto/simple/pub.c
+++ proto/simple/pub.c
@@ -13,9 +13,10 @@ int main (int argc, char *argv [])
     void *buf;                     // Buffer to store serialized data
     unsigned len;
 
-    command.ip = "192.168.1.1";
+    
     command.port = 503;
     command.id = 22;
+    command.ip = "192.168.1.1";
     len = main__mb_tcp_header__get_packed_size(&command);
     buf = malloc(len);
     main__mb_tcp_header__pack(&command, buf);
