e822e77c2005cf0744b737f3c2d9dda5a8df159c
Taka Wang <wang.chia.ming@gmail.com>
2016-03-02 13:35:18 +0800

    implement pub/sub golang

---
+++ proto/repeat/pub.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	_ "encoding/json"
+	"fmt"
+	proto "github.com/golang/protobuf/proto"
+	zmq "github.com/taka-wang/zmq3"
+	"time"
+)
+
+func main() {
+	pub()
+}
+
+func pub() {
+	sender, _ := zmq.NewSocket(zmq.PUB)
+	defer sender.Close()
+	sender.Connect("ipc:///tmp/dummy")
+
+	command := MbTcpMultipleWriteReq{ // named key
+		CmdHeader: CmdHeader{
+			Receiver: "core",
+			Sender:   "me",
+			Version:  "1",
+			Tid:      32,
+			Method:   "mbtcp.once.write",
+		},
+		MbTcpHeader: MbTcpHeader{
+			IP:   "192.168.1.1",
+			Port: 503,
+			ID:   22,
+		},
+	}
+
+	command.Requests = append(command.Requests,
+		MbWriteRequest{
+			Code:     1,
+			Register: 2003,
+			Value:    "1025",
+			Type:     "int64",
+			Alias:    "hello",
+		},
+	)
+	command.Requests = append(command.Requests,
+		MbWriteRequest{
+			Code:     1,
+			Register: 2003,
+			Value:    "1025",
+			Type:     "int64",
+			Alias:    "hello",
+		},
+	)
+
+	cmd, err := proto.Marshal(command)
+	if err != nil {
+		fmt.Println("marshaling error: ", err)
+	}
+
+	for {
+		sender.Send("mbtcp.once.write", zmq.SNDMORE)
+		sender.Send(string(cmd), 0)
+		time.Sleep(time.Duration(1000) * time.Millisecond)
+	}
+}
