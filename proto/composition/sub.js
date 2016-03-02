41781d8ec2a5537e781c501439cacc4d811c5395
Taka Wang <wang.chia.ming@gmail.com>
2016-03-02 09:44:13 +0800

    implement composistion pub/sub in node.js for protofbuf

---
+++ proto/composition/sub.js
@@ -0,0 +1,14 @@
+var zmq = require('zmq')
+, protobuf = require('protocol-buffers')
+, fs = require('fs')
+, messages = protobuf(fs.readFileSync('type.proto'))
+, sub = zmq.socket('sub')
+
+sub.bindSync("ipc:///tmp/dummy"); 	// bind to zmq endpoint
+sub.subscribe("mbtcp"); 			// filter topic
+
+sub.on("message", function(method, payload) {
+    console.log(method.toString());
+    var obj = messages.MbWriteRequest.decode(payload); //decode
+    console.dir(obj);
+});
