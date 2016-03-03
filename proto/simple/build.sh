bbeaedda6875c052b59a95dea2ba868d73d4bbdf
Taka Wang <wang.chia.ming@gmail.com>
2016-03-03 11:33:12 +0800

    update build script

--- proto/simple/build.sh
+++ proto/simple/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-rm pubc subc
+rm pubc subc pub sub
 gcc type2.pb-c.h type2.pb-c.c pub.c -lzmq -lczmq -lprotobuf-c  -o pubc
 gcc type2.pb-c.h type2.pb-c.c sub.c -lzmq -lczmq -lprotobuf-c  -o subc
+
+go build -o pub pub.go type.pb.go
+go build -o sub sub.go type.pb.go
