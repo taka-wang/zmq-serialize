41868e920790dac645160e028cbe9c3807c3117f
Taka Wang <wang.chia.ming@gmail.com>
2016-03-03 15:16:10 +0800

    clean code

--- protobuf/repeat/sub.c
+++ protobuf/repeat/sub.c
@@ -13,6 +13,7 @@ int main (int argc, char *argv [])
     
     while (!zctx_interrupted) {
         zmsg_t *msg = zmsg_recv(subscriber);
+        //zmsg_dump(msg);
         zframe_t *first = zmsg_pop(msg);
         zframe_t *payload = zmsg_pop(msg);
         char *first_buffer = zframe_strdup(first);
@@ -23,10 +24,9 @@ int main (int argc, char *argv [])
         // unpack
         MbTcpMultipleWriteReq *command;
         
-        CmdHeader *cmd_header;
-        MbTcpHeader *mb_tcp_header;
-        
-        MbWriteRequest **mb_write_requests; // note!
+        CmdHeader       *cmd_header;
+        MbTcpHeader     *mb_tcp_header;
+        MbWriteRequest  **mb_write_requests; // note!
 
         unsigned len = zframe_size(payload);
 
@@ -46,14 +46,6 @@ int main (int argc, char *argv [])
         int i = 0;
         for (i = 0; i < command->n_requests; ++i)
         {
-            /*
-            printf("Recv: %d %d %s %s %s\n",
-                command->requests[i]->code, 
-                command->requests[i]->register_, 
-                command->requests[i]->value, 
-                command->requests[i]->type, 
-                command->requests[i]->alias);
-            */
             printf("Recv: %d %d %s %s %s\n",
                 mb_write_requests[i]->code, 
                 mb_write_requests[i]->register_, 
@@ -61,8 +53,6 @@ int main (int argc, char *argv [])
                 mb_write_requests[i]->type, 
                 mb_write_requests[i]->alias);
         }
-
-        //zmsg_dump(msg);
         
         //cleanup
         mb_tcp_multiple_write_req__free_unpacked(command, NULL); // free unpacked command
