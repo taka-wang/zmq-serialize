46b93d0f204f5723351679054e38043aee8323b0
Taka Wang <wang.chia.ming@gmail.com>
2016-03-03 11:47:06 +0800

    refine proto2

---
+++ proto/composition/pub.c
@@ -0,0 +1,56 @@
+// compile: gcc type2.pb-c.h type2.pb-c.c pub.c -lzmq -lczmq -lprotobuf-c  -o pubc
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "type2.pb-c.h"
+#include <czmq.h>
+#include <unistd.h>
+
+
+int main (int argc, char *argv [])
+{
+
+    void *buf; // Buffer to store serialized data
+    unsigned len;
+
+    Main__MbTcpHeader command = MAIN__MB_TCP_HEADER__INIT; // construct
+
+    Main__CmdHeader
+    Main__MbTcpHeader
+    Main__MbWriteRequest
+    Main__MbTcpSingleWriteReq
+
+    MAIN__CMD_HEADER__INIT
+    MAIN__MB_TCP_HEADER__INIT
+    MAIN__MB_WRITE_REQUEST__INIT
+    MAIN__MB_TCP_SINGLE_WRITE_REQ__INIT
+
+    //command.has_port = 1;
+    //command.has_id = 1;
+    command.ip = "192.168.1.1";
+    command.port = 503;
+    command.id = 22;
+    
+    len = main__mb_tcp_header__get_packed_size(&command);
+    
+    buf = malloc(len);
+    main__mb_tcp_header__pack(&command, buf);
+
+    zctx_t *context = zctx_new();
+    void *publisher = zsocket_new(context, ZMQ_PUB);
+    zsocket_connect (publisher, "ipc:///tmp/dummy");
+    
+    while (!zctx_interrupted) { // handle ctrl+c
+        zmsg_t *msg = zmsg_new();
+        zmsg_addstr(msg, "mbtcp.once.write");   // frame 1
+        zmsg_addstr(msg, (char*)buf);           // frame 2
+        zmsg_send(&msg, publisher);
+        zclock_sleep(1000); // sleep 1 second
+        zmsg_destroy(&msg);
+    }
+    
+    // cleanup
+    free(buf);
+    zctx_destroy(&context);
+    return 0;
+}
