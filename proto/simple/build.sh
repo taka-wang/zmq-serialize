88daed45140fb19eae53cb767692dd6e1b408fd9
Taka Wang <wang.chia.ming@gmail.com>
2016-03-03 14:46:52 +0800

    try modulize

--- proto/simple/build.sh
+++ proto/simple/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 rm pubc subc pub sub
+
+# build c
 gcc type2.pb-c.h type2.pb-c.c pub.c -lzmq -lczmq -lprotobuf-c  -o pubc
 gcc type2.pb-c.h type2.pb-c.c sub.c -lzmq -lczmq -lprotobuf-c  -o subc
 
+# build golang
 go build -o pub pub.go type.pb.go
 go build -o sub sub.go type.pb.go
--- proto/simple/pub.c
+++ proto/simple/pub.c
@@ -7,24 +7,28 @@
 #include <unistd.h>
 
 
-int main (int argc, char *argv [])
+void *buf pack()
 {
-
     void *buf; // Buffer to store serialized data
     unsigned len;
 
     Main__MbTcpHeader command = MAIN__MB_TCP_HEADER__INIT; // construct
 
-    //command.has_port = 1;
-    //command.has_id = 1;
+    // assignment
     command.ip = "192.168.1.1";
     command.port = 503;
     command.id = 22;
     
-    len = main__mb_tcp_header__get_packed_size(&command);
-    
-    buf = malloc(len);
-    main__mb_tcp_header__pack(&command, buf);
+    len = main__mb_tcp_header__get_packed_size(&command); // get length
+    buf = malloc(len);                                    // allocate memory
+    main__mb_tcp_header__pack(&command, buf);             // do pack
+
+    return buf;
+}
+
+int main (int argc, char *argv [])
+{
+    void *buf = pack();
 
     zctx_t *context = zctx_new();
     void *publisher = zsocket_new(context, ZMQ_PUB);
@@ -33,7 +37,7 @@ int main (int argc, char *argv [])
     while (!zctx_interrupted) { // handle ctrl+c
         zmsg_t *msg = zmsg_new();
         zmsg_addstr(msg, "mbtcp.once.write");   // frame 1
-        zmsg_addstr(msg, (char*)buf);           // frame 2
+        zmsg_addstr(msg, (char*)buf);           // convert to char pointer: frame 2
         zmsg_send(&msg, publisher);
         zclock_sleep(1000); // sleep 1 second
         zmsg_destroy(&msg);
